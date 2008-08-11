57783bcb17ff2fb9c1cf9c0cd5b8790244b643cc
Filip Miletic <filmil@gmail.com>
2008-08-11 02:40:54 +0200

    Started the transfer to Mercurial for FF 3.1 and friends
    - Corrected scripts to point to new repositories.
    - Made the new repos configurable
    - Started the changingo of absolute paths in the scripts and config files
      to relative paths.

--- release-3/scripts/make-pot-from-file.sh
+++ release-3/scripts/make-pot-from-file.sh
@@ -3,7 +3,7 @@
 # Gets a file name, and makes a POT file in the working directory.
 # The first argument is the full file name relative to the base
 # directory of the l10n tree.
-TEMPLATE_LOC=~/personal/l10n/mozilla-cvs/3/template/en-US
+. scripts/config.sh
 SOURCE_FILE=$1
 OUT_FILE=NEWPOT.pot
 SOURCE=$TEMPLATE_LOC/$SOURCE_FILE
--- release-3/scripts/scripts-config.sh
+++ release-3/scripts/scripts-config.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
 WORKING_DIR=~/personal/l10n/work/mozilla/release-3
+TEMPLATE_LOC=... 		#  FIXME
