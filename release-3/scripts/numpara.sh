b23f29339c595afb5eaf4cb620dddde581454a84
Filip Miletic <filmil@gmail.com>
2008-08-20 00:44:32 +0200

    Removing extraneous release-3 from the all-2.0.x branch

--- release-3/scripts/numpara.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. scripts/library.sh
-
-PARAM_STR="1 2"
-
-num_params 1 2
-num_params $PARAM_STR
--- release-3/scripts/scripts-config.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-WORKING_DIR=~/personal/l10n/work/mozilla/release-3
-TEMPLATE_LOC=... 		#  FIXME
