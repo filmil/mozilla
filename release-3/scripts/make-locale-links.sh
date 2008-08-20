34d5e198318a0577a7eaf63c338224c9087b6d12
Filip Miletic <filmil@gmail.com>
2008-08-20 02:38:38 +0200

    Updated the files for ff-3.1

--- release-3/scripts/make-locale-links.sh
+++ release-3/scripts/make-locale-links.sh
@@ -31,22 +31,22 @@ mkdir -p $2/en-US/extensions/cview
 mkdir -p $2/en-US/extensions/signonviewer
 mkdir -p $2/en-US/extensions/wallet
 
-ln -s $1/extensions/cck/browser/locales/en-US $2/en-US/extensions/cck/browser
-ln -s $1/extensions/cview/resources/locale/en-US $2/en-US/extensions/cview/resources
-ln -s $1/extensions/irc/locales/en-US $2/en-US/extensions/irc
+# ln -s $1/extensions/cck/browser/locales/en-US $2/en-US/extensions/cck/browser
+# ln -s $1/extensions/cview/resources/locale/en-US $2/en-US/extensions/cview/resources
+# ln -s $1/extensions/irc/locales/en-US $2/en-US/extensions/irc
 ln -s $1/extensions/metrics/locale/en-US $2/en-US/extensions/metrics
 ln -s $1/extensions/reporter/locales/en-US $2/en-US/extensions/reporter
 ln -s $1/extensions/spellcheck/locales/en-US $2/en-US/extensions/spellcheck
-ln -s $1/extensions/venkman/locales/en-US $2/en-US/extensions/venkman
+# ln -s $1/extensions/venkman/locales/en-US $2/en-US/extensions/venkman
 
 mkdir -p $2/en-US/extensions/wallet/editor
 mkdir -p $2/en-US/extensions/wallet/src
 
-ln -s $1/extensions/wallet/src/resources/locale/en-US $2/en-US/extensions/wallet/src/resources
-ln -s $1/extensions/wallet/resources/locale/en-US $2/en-US/extensions/wallet/resources
+# ln -s $1/extensions/wallet/src/resources/locale/en-US $2/en-US/extensions/wallet/src/resources
+# ln -s $1/extensions/wallet/resources/locale/en-US $2/en-US/extensions/wallet/resources
 
 # Mail
-ln -s $1/mail/locales/en-US $2/en-US/mail
+# ln -s $1/mail/locales/en-US $2/en-US/mail
 
 # Netwerk
 ln -s $1/netwerk/locales/en-US $2/en-US/netwerk
@@ -54,7 +54,7 @@ ln -s $1/netwerk/locales/en-US $2/en-US/netwerk
 # Other-licenses
 mkdir -p $2/en-US/other-licenses/branding
 
-ln -s $1/other-licenses/branding/thunderbird/locales/en-US $2/en-US/other-licenses/branding/thunderbird
+# ln -s $1/other-licenses/branding/thunderbird/locales/en-US $2/en-US/other-licenses/branding/thunderbird
 ln -s $1/other-licenses/branding/firefox/locales/en-US $2/en-US/other-licenses/branding/firefox
 
 # Security
@@ -63,10 +63,3 @@ ln -s $1/security/manager/locales/en-US $2/en-US/security/manager
 
 # Toolkit
 ln -s $1/toolkit/locales/en-US $2/en-US/toolkit
-
-
-
-
-
-
-
