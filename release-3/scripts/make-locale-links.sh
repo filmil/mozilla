603cf8e2678dc7563cd28a6bfae4ec0d024f4e8f
Filip Miletic <filmil@gmail.com>
2008-09-14 00:01:48 +0200

    Updated the link generation to take the links from comm-central.
    From now on, the links are taken from the comm-central, which also
    pulls in the Firefox files as a separate directory, mozilla/

--- release-3/scripts/make-locale-links.sh
+++ release-3/scripts/make-locale-links.sh
@@ -15,11 +15,11 @@ mkdir -p $2
 
 # Browser
 mkdir -p $2/en-US
-ln -s $1/browser/locales/en-US $2/en-US/browser
+ln -s $1/mozilla/browser/locales/en-US $2/en-US/browser
 
 # DOM
 mkdir -p $2/en-US/dom
-ln -s $1/dom/locales/en-US/chrome $2/en-US/dom/chrome
+ln -s $1/mozilla/dom/locales/en-US/chrome $2/en-US/dom/chrome
 
 # Editor
 mkdir -p $2/en-US/editor/ui
@@ -34,33 +34,33 @@ mkdir -p $2/en-US/extensions/wallet
 
 # ln -s $1/extensions/cck/browser/locales/en-US $2/en-US/extensions/cck/browser
 # ln -s $1/extensions/cview/resources/locale/en-US $2/en-US/extensions/cview/resources
-# ln -s $1/extensions/irc/locales/en-US $2/en-US/extensions/irc
-ln -s $1/extensions/metrics/locale/en-US $2/en-US/extensions/metrics
-ln -s $1/extensions/reporter/locales/en-US $2/en-US/extensions/reporter
-ln -s $1/extensions/spellcheck/locales/en-US $2/en-US/extensions/spellcheck
-# ln -s $1/extensions/venkman/locales/en-US $2/en-US/extensions/venkman
+ln -s $1/mozilla/extensions/irc/locales/en-US $2/en-US/extensions/irc
+ln -s $1/mozilla/extensions/metrics/locale/en-US $2/en-US/extensions/metrics
+ln -s $1/mozilla/extensions/reporter/locales/en-US $2/en-US/extensions/reporter
+ln -s $1/mozilla/extensions/spellcheck/locales/en-US $2/en-US/extensions/spellcheck
+ln -s $1/mozilla/extensions/venkman/locales/en-US $2/en-US/extensions/venkman
 
 mkdir -p $2/en-US/extensions/wallet/editor
 mkdir -p $2/en-US/extensions/wallet/src
 
-# ln -s $1/extensions/wallet/src/resources/locale/en-US $2/en-US/extensions/wallet/src/resources
-# ln -s $1/extensions/wallet/resources/locale/en-US $2/en-US/extensions/wallet/resources
+ln -s $1/mozilla/extensions/wallet/src/resources/locale/en-US $2/en-US/extensions/wallet/src/resources
+ln -s $1/mozilla/extensions/wallet/resources/locale/en-US $2/en-US/extensions/wallet/resources
 
 # Mail
-# ln -s $1/mail/locales/en-US $2/en-US/mail
+ln -s $1/mail/locales/en-US $2/en-US/mail
 
 # Netwerk
-ln -s $1/netwerk/locales/en-US $2/en-US/netwerk
+ln -s $1/mozilla/netwerk/locales/en-US $2/en-US/netwerk
 
 # Other-licenses
 mkdir -p $2/en-US/other-licenses/branding
 
-# ln -s $1/other-licenses/branding/thunderbird/locales/en-US $2/en-US/other-licenses/branding/thunderbird
-ln -s $1/other-licenses/branding/firefox/locales/en-US $2/en-US/other-licenses/branding/firefox
+ln -s $1/other-licenses/branding/thunderbird/locales/en-US $2/en-US/other-licenses/branding/thunderbird
+ln -s $1/mozilla/other-licenses/branding/firefox/locales/en-US $2/en-US/other-licenses/branding/firefox
 
 # Security
 mkdir -p $2/en-US/security
-ln -s $1/security/manager/locales/en-US $2/en-US/security/manager
+ln -s $1/mozilla/security/manager/locales/en-US $2/en-US/security/manager
 
 # Toolkit
-ln -s $1/toolkit/locales/en-US $2/en-US/toolkit
+ln -s $1/mozilla/toolkit/locales/en-US $2/en-US/toolkit
