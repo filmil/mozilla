af11a04642e398f842f9073c780fb9f76cc2d569
Filip Miletic <filmil@gmail.com>
2008-12-16 23:50:12 -0800

    Changed to support ff-3.2

--- release-3/scripts/scripts-config.sh
+++ release-3/scripts/scripts-config.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 WORKING_DIR=~/personal/l10n/work/mozilla/release-3
-TEMPLATE_LOC=~/personal/l10n/mozilla/ff-3.1/template/en-US
+TEMPLATE_LOC=~/personal/l10n/mozilla/ff-3.2/template/en-US
