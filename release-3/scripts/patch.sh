b776e8c43f8fb46387554dd21dc3ed6338b96199
Filip Miletic <filmil@gmail.com>
2011-05-26 01:20:26 -0700

    Remove release 3 files.

--- release-3/scripts/patch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Patching Translate Toolkit quirks"
-
-echo "reporterMenu.accessKey quirk"
-INFILE=output/sr/extensions/reporter/chrome/reporterOverlay.dtd
-TEMPFILE=output/sr/extensions/reporter/chrome/reporterOverlay.dtd.1
-sed -e "s/reporterMenu.accesskey \"R\"/reporterMenu.accesskey\ \"П\"/" < $INFILE > $TEMPFILE
-mv $TEMPFILE $INFILE
--- release-3/scripts/untranslated.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Makes a list of untranslated messages
-STATS_DIR=tools/stats
-echo "Making a list of untranslated files..."
-grep -i untranslated $STATS_DIR/stats.txt 
